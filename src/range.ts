1ebfc83e72e4cfe748f177bc29f76585ea5d6f1a
Vitaliy Komarov <mkots@yandex.ru>
2021-06-13 22:50:25 +0300

    feat: add range and sum of array function

---
+++ src/range.ts
@@ -0,0 +1,10 @@
+export const range = (start: number, stop: number, step: number = 1): Array<number> => {
+    let length = Math.max(Math.ceil((stop - start) / step), 0);
+    const arr: Array<number> = [];
+
+    while (length--){
+        arr.push(start);
+        start += step
+    }
+    return arr;
+};
