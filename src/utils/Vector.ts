a3e80580ed2d36bd2afc0b01a761f163de74f82d
Vitaliy Komarov <36552393+Mkots@users.noreply.github.com>
2023-03-23 00:13:34 +0300

    Vector (#157)
    * Add the Vector class
    
    * Add more tests for Vector
    
    ---------
    
    Co-authored-by: komarov-v <unumverumbonum@yandex.ru>

---
+++ src/utils/Vector.ts
@@ -0,0 +1,89 @@
+export class Vector extends Array<number>{
+    private items: Array<number>;
+    constructor(...items: Array<number>) {
+        super(...items)
+        this.items = items;
+    }
+
+    static #isEqual(a: number, b: number, treshold = 0.01) {
+        return Math.abs(a - b) < treshold;
+    }
+    static #toDegrees(radians: number) {
+        return (radians * 180) / Math.PI;
+    }
+    add(vector: Vector) {
+        if (vector.size !== this.size) {
+            throw new Error("Vectors should be the same size");
+        }
+        return new Vector(
+            ...vector.items.map((value, index) => this.items[index] + value),
+        );
+    }
+    sub(vector: Vector) {
+        if (vector.size !== this.size) {
+            throw new Error("Vectors should be the same size");
+        }
+        return new Vector(
+            ...vector.items.map((value, index) => this.items[index] - value),
+        );
+    }
+    scaleBy(number: number) {
+        return new Vector(
+            ...this.items.map((value) => value * number),
+        );
+    }
+    dotProduct(vector: Vector) {
+        if (vector.size !== this.size) {
+            throw new Error("Vectors should be the same size");
+        }
+        return vector.items.reduce((acc, value, index) => {
+            return acc + value * this.items[index];
+        }, 0);
+    }
+    normalize() {
+        return this.scaleBy(1 / this.modulus);
+    }
+
+    haveSameDirectionWith(vector: Vector) {
+        const dotProd = this.normalize().dotProduct(vector.normalize());
+        return Vector.#isEqual(dotProd, 1);
+    }
+    haveOppositeDirectionWith(vector: Vector) {
+        const dotProd = this.normalize().dotProduct(vector.normalize());
+        return Vector.#isEqual(dotProd, -1);
+    }
+    isPerpendicular(vector: Vector) {
+        const dotProd = this.normalize().dotProduct(vector.normalize());
+        return Vector.#isEqual(dotProd, 0);
+    }
+    angleBetween(vector: Vector) {
+        return Vector.#toDegrees(Math.acos(
+            this.dotProduct(vector) / (this.modulus * vector.modulus),
+        ));
+    }
+    isEqualTo(vector: Vector) {
+        if (vector.size !== this.size) {
+            throw new Error("Vectors should be the same size");
+        }
+        return vector.items.every((value, index) =>
+            Vector.#isEqual(value, this.items[index])
+        );
+    }
+
+    get modulus() {
+        return Math.hypot(...this.items);
+    }
+
+    get size() {
+        return this.items.length;
+    }
+    get x() {
+        return this.items[0];
+    }
+    get y() {
+        return this.items[1];
+    }
+    get z() {
+        return this.items[2];
+    }
+}
